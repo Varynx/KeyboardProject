491ad1b4165cbcb17f881027b25e5d4d73f1abed
Bau-t <108375706+Bau-t@users.noreply.github.com>
2024-11-20 14:05:19 -0500

    Update keyboard.cpp
    Initialized each black and white key  and their properties
    (size, color, shape, position, etc.)

--- keyboard.cpp
+++ keyboard.cpp
@@ -3,12 +3,58 @@
 #include <SFML/Audio.hpp>
 
 // Constructor definition
-Keyboard::Keyboard() : keyboardWindow(sf::VideoMode(800, 600), "Keyboard Window") {
+Keyboard::Keyboard() : keyboardWindow(sf::VideoMode(800, 600), "Keyboard Window") 
+{
     keyboardWindow.setFramerateLimit(60);
     buffer.loadFromFile("../assets/sounds/a3.mp3");
     sound.setBuffer(buffer);
     buffer1.loadFromFile("../assets/sounds/b3.mp3");
-    sound1.setBuffer(buffer1);;
+    sound1.setBuffer(buffer1);
+
+    //loads jpg image from computer (change filepath as needed)
+    backgroundTexture.loadFromFile("C:/Users/bauti/KeyboardProjectFiles/randomMinimalistBackground.jpg");
+    backgroundImage.setTexture(&backgroundTexture);
+    backgroundImage.setSize(sf::Vector2f(2400,1600));
+
+    //loads in gradient for white and black key textures (change filepath as needed)
+    whiteKeyTexture.loadFromFile("C:/Users/bauti/KeyboardProjectFiles/beigeToWhite.jpg");
+    blackKeyTexture.loadFromFile("C:/Users/bauti/KeyboardProjectFiles/blackGradientTransparent.jpg");
+
+    //WHITE KEYS INITIALIZED BELOW
+    float xPositionWhiteKeys = 0;
+    float yPositionWhiteKeys = 700;
+    for(int iter = 0; iter < 36; iter++)
+    {
+        keyboardNotes[iter].setPosition(xPositionWhiteKeys,yPositionWhiteKeys);
+        keyboardNotes[iter].setSize(sf::Vector2f(66.6,400));
+        keyboardNotes[iter].setFillColor(sf::Color::White);
+        keyboardNotes[iter].setOutlineThickness(2);
+        keyboardNotes[iter].setOutlineColor(sf::Color::Black);
+        keyboardNotes[iter].setTexture(&whiteKeyTexture);
+
+        xPositionWhiteKeys += 66.6;
+    }
+
+     //BLACK KEYS INITIALIZED BELOW
+    float xPositionBlackKeys = 45.3;
+    float yPositionBlackKeys = 700;
+
+    std::set<int> xPositionSkip = {38,41,43,46,48,51,53,56,58};
+    for(int iterator = 36; iterator < 61; iterator++)
+    {
+        if(xPositionSkip.find(iterator) != xPositionSkip.end())
+        {
+            xPositionBlackKeys += 66.6;
+        }
+        keyboardNotes[iterator].setPosition(xPositionBlackKeys,yPositionBlackKeys);
+        keyboardNotes[iterator].setSize(sf::Vector2f(40,200));
+        keyboardNotes[iterator].setFillColor(sf::Color::Black);
+        keyboardNotes[iterator].setOutlineThickness(2);
+        keyboardNotes[iterator].setOutlineColor(sf::Color::Black);
+        keyboardNotes[iterator].setTexture(&blackKeyTexture);
+
+        xPositionBlackKeys += 66.6;
+    }
 }
 
 void Keyboard::keyboardEvent() {//test for events

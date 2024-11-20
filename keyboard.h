10912273239a7b805a6bb8410556f33fe62bf2d1
Gustavo Vega <gustavovega.ab@gmail.com>
2024-11-20 18:59:32 -0500

    combined code

--- keyboard.h
+++ keyboard.h
@@ -3,12 +3,17 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include <array>
 
 class Keyboard {
     sf::RenderWindow keyboardWindow;
     sf::SoundBuffer pianoBuffer[7];
     sf::Sound pianoSound[7];
-
+    sf::RectangleShape backgroundImage;
+    sf::Texture backgroundTexture;
+    std::array<sf::RectangleShape,61> keyboardNotes;
+    sf::Texture whiteKeyTexture;
+    sf::Texture blackKeyTexture;
 public:
     // Constructor
 
